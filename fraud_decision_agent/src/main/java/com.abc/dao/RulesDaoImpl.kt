c43f0ed4db778a5773ebf7dcb98919492b6808d6
Ihor Prokopov <Ihor_Prokopov@epam.com>
2017-02-05 15:08:08 +0200

    Add mock to rules repo

--- fraud_decision_agent/src/main/java/com.abc/dao/RulesDaoImpl.kt
+++ fraud_decision_agent/src/main/java/com.abc/dao/RulesDaoImpl.kt
@@ -1,11 +1,13 @@
 package com.abc.dao
 
+import com.abc.model.decision.MaxMin
 import com.abc.model.decision.Rule
 import org.springframework.stereotype.Repository
 
 @Repository
 class RulesDaoImpl : RulesDao {
     override fun getUsersRule(userId: String): List<Rule> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listOf(Rule(listOf(MaxMin(100, 0)), listOf("US"), listOf("UA"), listOf(MaxMin(100, 0)),
+                listOf(MaxMin(100, 0)), listOf(MaxMin(100, 0)), 100))
     }
 }
