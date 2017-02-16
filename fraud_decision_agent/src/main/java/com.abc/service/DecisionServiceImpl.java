41f192bae62ae4e5751322e499643b3e0d0cf83c
Ihor Prokopov <Ihor_Prokopov@epam.com>
2017-02-16 23:02:45 +0200

    remove extra main method

--- fraud_decision_agent/src/main/java/com.abc/service/DecisionServiceImpl.java
+++ fraud_decision_agent/src/main/java/com.abc/service/DecisionServiceImpl.java
@@ -23,35 +23,6 @@ public class DecisionServiceImpl implements DecisionService {
     @Autowired
     private RulesDAO rulesDao;
 
-    public static void main(String[] args) {
-        List results = Lists.newArrayList(new EmailAgeResponse.Results("", null, null, null,
-                null, null, null, null, "", null,
-                String.valueOf(new Random().nextInt(1001)), null, null, null, null,
-                null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null,
-                null, null, null, null, null,
-                null, null, null, null, null,
-                null, null, null, null, null, null,
-                null, null, null, null, null, null,
-                null, null, null, null, null, null,
-                null, null, null, null, null, null,
-                null, null, null, null, null, null,
-                null, null, null));
-
-        EmailAgeResponse res =  (new EmailAgeResponse(UUID.randomUUID(), 0, new EmailAgeResponse.Query("", null, 0, null,
-                null, null, results), (EmailAgeResponse.ResponseStatus) null));
-
-        VendorResponse[] responses = new VendorResponse[]{res};
-        DecisionServiceImpl target = new DecisionServiceImpl();
-        RuleRequest ruleRequest = new RuleRequest(new VendorResponse[0]);
-        List<Rule> rules = Lists.newArrayList(new Rule());
-        rules.get(0).setZumigoLN(Lists.newArrayList(new Rule.MaxMin(79, 0)));
-        rules.get(0).setZumigoFN(Lists.newArrayList(new Rule.MaxMin(79, 0)));
-        rules.get(0).setZumigoAdress(Lists.newArrayList(new Rule.MaxMin(79, 0)));
-        DecisionResponse response = target.makeDecision(ruleRequest, rules, UUID.randomUUID());
-        System.out.println(response.getScore());
-    }
-
     @Override
     public DecisionResponse makeDecision(VendorResponse[] vendorResponses) {
         List<Rule> rules = rulesDao.findByUserId(vendorResponses[0].getUserId());
